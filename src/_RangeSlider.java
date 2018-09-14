4b606bcc76470913adc83827785d805a6f3c9ac0
root <smail.bamba@gmail.com>
2018-09-14 11:14:29 +0200

    interface RangeSlider

---
+++ src/_RangeSlider.java
@@ -0,0 +1,26 @@
+
+public interface _RangeSlider {
+	
+	int getMinRange();
+	
+	int setMinRange();
+	
+	int getMaxRange();
+	
+	int setMaxRange();
+	
+	int getValue();
+	
+	int setValue();
+	
+	int getMin();
+	
+	int setMin();
+	
+	int getMax();
+	
+	int setMax();
+	
+	
+
+}
