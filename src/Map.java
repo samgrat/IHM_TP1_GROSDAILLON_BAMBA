5de2187b3469ea486b04c4eee16f136b7f04589f
root <smail.bamba@gmail.com>
2018-09-28 18:53:24 +0200

    Bonus implementé et fonctionnel, ajout de quelques commentaires

--- src/Map.java
+++ src/Map.java
@@ -3,14 +3,19 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.MouseInputAdapter;
+import javax.swing.plaf.basic.BasicSliderUI.TrackListener;
 
 // https://coderanch.com/t/338737/java/draw-points-Java
 public class Map extends JPanel {
-	
+
 	/**
 	 * 
 	 */
@@ -19,44 +24,182 @@ public class Map extends JPanel {
 	private ArrayList<Home> listeMaisons;
 	private RangeSlider slider_pieces;
 	private RangeSlider slider_prix;
-	
-	
+	private JSlider slider_rayonA;
+	private JSlider slider_rayonB;
+
+	private Ellipse2D point_A;
+	private Ellipse2D point_B;
+
+	private enum Etats {
+		IDLE, PLACE_A, PAUSE, PLACE_B
+	};
+
+	private Etats etat;
+
 	// Quelques constantes graphiques
+	private static final int TAILLE_A_B = 4;
+	private static final int TAILLE_CARTE = 400;
+	private static final Color COULEUR_A = Color.red;
+	private static final Color COULEUR_B = Color.blue;
 	private static final Color COULEUR_POINTS = Color.yellow;
 	private static final Color COULEUR_FOND = Color.black;
-	
-	public Map( ArrayList<Home> maisons, RangeSlider nombre_de_pieces, RangeSlider valeur_maison) {
-		//super(borderLayout);
-		//setBackground(COULEUR_FOND);
+
+	public Map(ArrayList<Home> maisons, RangeSlider nombre_de_pieces, RangeSlider valeur_maison, JSlider slider_rayonA, JSlider slider_rayonB) {
+		// super();
 		listeMaisons = maisons;
-		setBounds(0,0,100,100);
 		this.slider_pieces = nombre_de_pieces;
 		this.slider_prix = valeur_maison;
+		this.slider_rayonA = slider_rayonA;
+		this.slider_rayonB = slider_rayonB;
+
+		this.etat = Etats.IDLE;
+		this.point_A = null;
+		this.point_B = null;
+
+		// On ajoute notre Mouse listener
+		addMouseListener(new ClicEventListener());
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		g.setColor(COULEUR_FOND);
+		g.fillRect(0, 0, TAILLE_CARTE, TAILLE_CARTE);
+		Graphics2D g2 = (Graphics2D) g;
+		Home maison;
+		// On initialise les distance au minimum
+		double distanceA = 0;
+		double distanceB = 0;
+
+		Ellipse2D e;
+
+		// On dessine A et B si ils existent
+		if (point_A != null) {
+			g.setColor(COULEUR_A);
+			g2.draw(point_A);
+		}
+
+		if (point_B != null) {
+			g.setColor(COULEUR_B);
+			g2.draw(point_B);
+		}
+
+		for (int j = 0; j < listeMaisons.size(); j++) {
+			maison = listeMaisons.get(j);
+			
+			// On calcule les distance si A et B existent
+			if (point_A != null) {
+				distanceA =DistanceElipses(maison.getX(), maison.getY(), point_A);
+			}
+			if (point_B != null) {
+				distanceB =DistanceElipses(maison.getX(), maison.getY(), point_B);
+			}
+
+			// On dessine les maisons si elles ont un nombre de pieces, un prix, un distance par rapport a A et a B valide
+			if (maison.getPieces() >= slider_pieces.getMinRange()
+					&& maison.getPieces() <= slider_pieces.getMaxRange()
+					&& maison.getValeur() >= slider_prix.getMinRange()
+					&& maison.getValeur() <= slider_prix.getMaxRange()
+					&& distanceA  <= slider_rayonA.getValue()
+					|| distanceB  <= slider_rayonB.getValue()) {
+				
+				e = new Ellipse2D.Double((maison).getX(), (maison).getY(), 1, 1);
+				g.setColor(COULEUR_POINTS);
+				g2.draw(e);
+
+			}
+
+		}
+
 	}
 	
-	@Override
-	protected void paintComponent(Graphics g)
-    {
-		g.setColor(COULEUR_FOND);
-        g.fillRect(0, 0, 400, 400);
-        System.out.println("heeey");
-       Graphics2D g2 = (Graphics2D)g;
-        
-        Ellipse2D e;
-        
-        for(int j = 0; j < listeMaisons.size(); j++)
-        {
-        	if (listeMaisons.get(j).getPieces() >= slider_pieces.getMinRange() && 
-        			listeMaisons.get(j).getPieces() <= slider_pieces.getMaxRange() &&
-        			listeMaisons.get(j).getValeur() >= slider_prix.getMinRange() &&
-        			listeMaisons.get(j).getValeur() <= slider_prix.getMaxRange()) {
-                e = new Ellipse2D.Double((listeMaisons.get(j)).getX(), (listeMaisons.get(j)).getY(), 1, 1);
-                g.setColor(COULEUR_POINTS);
-                g2.draw(e);
-                
-        	}
+	/**
+	 * Calcule la distance entre deux coordonnee et un elipse
+	 * @param x
+	 * @param y
+	 * @param e2
+	 * @return
+	 */
+	private double DistanceElipses(double x, double y, Ellipse2D e2) {
+		
+		return Math.sqrt((e2.getX() - x) * (e2.getX() - x) + (e2.getY() - y)*(e2.getY() - y));
+	}
 
-        }
+	private class ExceptionEtat extends Exception {
+		/**
+		* 
+		*/
+		private static final long serialVersionUID = 1L;
 
-    }
+		public ExceptionEtat(MouseEvent e) {
+			System.err.println("Evenement non capturé, etat courant = " + etat + " event = " + e);
+		}
+
+	}
+
+	private class ClicEventListener extends MouseInputAdapter {
+
+		// Machine a etat pour le placement successif des points A et B
+		//
+		// |IDLE| -mousePressed-> |PLACE_A @(mouseDragged)| -mouseReleased-------
+		// /\ |
+		// L-mouseReleased- |PLACE_B @(mouseDragged)| <-mousePressed- |PAUSE|<
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			System.out.println("Pressed etat = " + etat);
+			switch (etat) {
+			case IDLE:
+				if (e.getX() <= TAILLE_CARTE && e.getY() <= TAILLE_CARTE) {
+					point_A = new Ellipse2D.Double(e.getX(), e.getY(), TAILLE_A_B, TAILLE_A_B);
+					repaint();
+					etat = Etats.PLACE_A;
+				} else {
+					System.err.println("Clic hors carte");
+				}
+				break;
+			case PAUSE:
+				if (e.getX() <= TAILLE_CARTE && e.getY() <= TAILLE_CARTE) {
+					point_B = new Ellipse2D.Double(e.getX(), e.getY(), TAILLE_A_B, TAILLE_A_B);
+					repaint();
+					etat = Etats.PLACE_B;
+				} else {
+					System.err.println("Clic hors carte");
+				}
+				break;
+			default:
+				new ExceptionEtat(e);
+			}
+		}
+
+		// TODO Debug n'est jamais enclenché
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			System.out.println("Dragged etat = " + etat);
+			switch (etat) {
+			case PLACE_A:
+				etat = Etats.PLACE_A;
+				break;
+			case PLACE_B:
+				etat = Etats.PLACE_B;
+				break;
+			default:
+				new ExceptionEtat(e);
+			}
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			System.out.println("Released etat = " + etat);
+			switch (etat) {
+			case PLACE_A:
+				etat = Etats.PAUSE;
+				break;
+			case PLACE_B:
+				etat = Etats.IDLE;
+				break;
+			default:
+				new ExceptionEtat(e);
+			}
+		}
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.plaf.basic.BasicSliderUI;
 
 public class Test {
@@ -21,9 +23,17 @@ public class Test {
 	
 	public static RangeSlider piecesSlider = new RangeSlider(PIECES_MIN, PIECES_MAX);
 	public static RangeSlider valeurSlider = new RangeSlider(VALEUR_MIN, VALEUR_MAX);
+	public static JSlider rayonASlider = new JSlider(JSlider.HORIZONTAL, 0, (int) X_MAX, 0);
+	public static JSlider rayonBSlider = new JSlider(JSlider.HORIZONTAL, 0, (int) X_MAX, 0);
 	
+	public static JLabel rayonA_title = new JLabel("Rayon A", SwingConstants.CENTER);
+	public static JLabel rayonB_title = new JLabel("Rayon B", SwingConstants.CENTER);
+	
+	public static JLabel pieces_title = new JLabel("Pieces", SwingConstants.CENTER);
 	public static JLabel pieces_min_max = new JLabel("min : " + piecesSlider.getMin() + " - max : " + piecesSlider.getMax(), SwingConstants.CENTER);
 	public static JLabel pieces_slider_values = new JLabel("min range : " + piecesSlider.getMinRange() + " - max range : " + piecesSlider.getMaxRange(), SwingConstants.CENTER);
+	
+	public static JLabel valeur_title = new JLabel("Prix", SwingConstants.CENTER);
 	public static JLabel valeur_min_max = new JLabel("min : " + valeurSlider.getMin() + " - max : " + valeurSlider.getMax(), SwingConstants.CENTER);
 	public static JLabel valeur_slider_values = new JLabel("min range : " + valeurSlider.getMinRange() + " - max range : " + valeurSlider.getMaxRange(), SwingConstants.CENTER);
 	
@@ -41,18 +51,37 @@ public class Test {
 				for(int i = 0; i < M; i++) {
 					ajouteMaison(listeMaisons);
 				}
-				Map map = new Map(listeMaisons, piecesSlider, valeurSlider);
+				
+				// Ensuite on cree notre carte
+				Map map = new Map(listeMaisons, piecesSlider, valeurSlider, rayonASlider, rayonBSlider);
 
 				//On crée une nouvelle instance de notre JDialog				
 				piecesSlider.setUI(new RangeSliderUI(piecesSlider, pieces_slider_values, map));
 				valeurSlider.setUI(new RangeSliderUI(valeurSlider, valeur_slider_values, map));		
+				rayonASlider.setUI(new BasicSliderUI(rayonASlider));
+				rayonBSlider.setUI(new BasicSliderUI(rayonASlider));
+				
+				// On ajoute un listener pour redessiner notre carte en cas de changement de valeur
+				rayonASlider.addChangeListener(new ChangeListener() {
+				      public void stateChanged(ChangeEvent event) {
+				    	  map.repaint();
+				      }
+				    });
+				rayonBSlider.addChangeListener(new ChangeListener() {
+				      public void stateChanged(ChangeEvent event) {
+				        map.repaint();
+				      }
+				    });
+				
 				
 				JPanel pieces_labels = new JPanel(new BorderLayout());
-				pieces_labels.add(pieces_min_max,BorderLayout.NORTH);	
+				pieces_labels.add(pieces_title,BorderLayout.NORTH);	
+				pieces_labels.add(pieces_min_max,BorderLayout.CENTER);	
 				pieces_labels.add(pieces_slider_values,BorderLayout.SOUTH);
 				
 				JPanel valeur_labels = new JPanel(new BorderLayout());
-				valeur_labels.add(valeur_min_max,BorderLayout.NORTH);	
+				valeur_labels.add(valeur_title,BorderLayout.NORTH);
+				valeur_labels.add(valeur_min_max,BorderLayout.CENTER);	
 				valeur_labels.add(valeur_slider_values,BorderLayout.SOUTH);
 				
 				// On creee des JPanel a part pour nos RangeSlider
@@ -64,13 +93,29 @@ public class Test {
 				slider2.add(valeur_labels, BorderLayout.NORTH);
 				slider2.add(valeurSlider, BorderLayout.SOUTH);
 				
+				// ainsi que nos Basic Sliders
+				JPanel slider3 = new JPanel(new BorderLayout());
+				slider3.add(rayonA_title, BorderLayout.NORTH);
+				slider3.add(rayonASlider, BorderLayout.SOUTH);
+
+				JPanel slider4 = new JPanel(new BorderLayout());
+				slider4.add(rayonB_title, BorderLayout.NORTH);
+				slider4.add(rayonBSlider, BorderLayout.SOUTH);
+				
+				// Puis un autre JPanel les englobant
+				JPanel sliders = new JPanel(new BorderLayout());
+				sliders.add(slider, BorderLayout.NORTH);
+				sliders.add(slider2, BorderLayout.SOUTH);
+				
+				// Et un autre
+				JPanel basicSliders = new JPanel(new BorderLayout());
+				basicSliders.add(slider3, BorderLayout.NORTH);
+				basicSliders.add(slider4, BorderLayout.SOUTH);
+					
 				// Puis un autre JPanel les englobant
 				JPanel controls = new JPanel(new BorderLayout());
-				controls.add(slider, BorderLayout.NORTH);
-				controls.add(slider2, BorderLayout.SOUTH);
-				
-				// Ensuite on cree notre carte TODO
-				
+				controls.add(sliders, BorderLayout.NORTH);
+				controls.add(basicSliders, BorderLayout.CENTER);
 				
 				// On organise nos composants
 				app.add(controls,BorderLayout.EAST);
